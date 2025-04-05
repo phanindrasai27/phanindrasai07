f36dee4e155746a14b11acbfa84990c31667204d
phanindrasai27 <bollarapuphanindra1375645@gmail.com>
2025-04-06 00:38:48 +0530

    Update Astro config for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://phanindrasai.space',
-  base: '/',
+  site: 'https://phanindrasai27.github.io',
+  base: '/phanindrasai07',
   build: {
     assets: '_assets'
   },
